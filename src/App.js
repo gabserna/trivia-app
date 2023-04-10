117edba12c79258074aeaac36cdd94b13ef61bf5
Gabriel Serna <109190797+gabserna@users.noreply.github.com>
2023-04-10 00:27:55 -0600

    formatting first

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>hello! it's me</p>
+        <p>por donde donde empirzo con esta trivia app ??</p>
       
       </header>
     </div>
