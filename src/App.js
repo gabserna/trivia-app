97dbc4835d52d009f832f12f7b1bc6f9183a76ad
Gabriel Serna <109190797+gabserna@users.noreply.github.com>
2023-04-10 00:29:06 -0600

    logo removed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>hello! it's me</p>
         <p>por donde donde empirzo con esta trivia app ??</p>
       
